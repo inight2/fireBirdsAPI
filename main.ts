6cc140edccdcb479624e6c5db45ca43d9c75483f
Israel <vasganight@gmail.com>
2018-12-09 20:03:49 -0600

    added the say and move methods

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
+class Init {
+    //name of the bot and current location
+    name: string;
+    currentX: number;
+    currentY: number;
+    constructor(name: string, x: number, y: number) {
+        this.name = name;
+        this.currentX = x;
+        this.currentY = y
+    }
+}
+
+class Bot extends Init {
+    say(message: string) {
+        return message + " " + this.name;
+    }
+    moveXY(x: number, y: number) {
+        //distance = radical Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)
+        let myDistance: number;
+        myDistance = Math.pow((this.currentX - x), 2) + Math.pow((this.currentY - y), 2);
+        myDistance = Math.sqrt(myDistance);
+        motors.largeAB.steer(0, 50, myDistance, MoveUnit.Rotations)
+        return myDistance;
+    }
+}
+
+
+// let bot = new Bot("George", 1, 2)
+// brick.showString(bot.say("lets fight"), 2);
+// bot.moveXY(10, 12);
+// brick.showNumber(bot.moveXY(10, 12), 1)
 
